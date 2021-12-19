577289a17d1571fb6b25cbfef8f50a079a9a2368
Daniel Barbosa de Souza <51936265+SouzaDaniel@users.noreply.github.com>
2021-12-19 16:19:37 -0300

    .

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,4 +1,4 @@
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDrrB9We9tVcHsIjQmiEC0sIyMCIgxVtz0&<?php if($logged): echo 'callback=initMap&'; endif; ?>libraries=&v=weekly"></script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDEBK2ffxcCXYVpu-Mt9WmkEtQX7sv0CkQ&<?php if($logged): echo 'callback=initMap&'; endif; ?>libraries=&v=weekly"></script>
     <script src="https://kit.fontawesome.com/113c46960f.js" crossorigin="anonymous"></script>
   </body>
 </html>
